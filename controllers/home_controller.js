a4b4d98dd6fc6cd39e63e54a4f9412c8a3f747da
harsh verma <harshv9988@gmail.com>
2020-06-27 17:17:12 +0530

    created home.ejs and setted up view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Server is Running</h1>');
+    return res.render('home',{
+        title:"home"
+    });
 }
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8000;
 //use express router
 app.use('/',require('./routes/index'));
+//setup view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`error in running server : ${err}`);
