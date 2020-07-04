2f44b35b4f377b63bfa27fea472b42a053ead2c4
harsh verma <harshv9988@gmail.com>
2020-07-04 17:25:06 +0530

    added user sign in and up page also added schema

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+
+//configuring the database
 const db = require('./config/mongoose')
+
+//importing the assets folder
 app.use(express.static('./assets'));
 
+//importing express-ejs-layout for same layout in each page
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 
+//these are used so that css file links goes into header after page reload
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
 //use express router
 app.use('/',require('./routes/index'));
+
 //setup view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`error in running server : ${err}`);
