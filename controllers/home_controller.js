6f0ebc4bd62562fb3520cf12d23a3583f73eb218
harshv9988 <harshv9988@gmail.com>
2020-07-20 13:11:02 +0530

    async await in home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,39 +1,90 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    // Post.find({},function(err,post){
-    //     if(err){
-    //         console.log('error in fetching');
-    //         return;
-    //     }
-    //     return res.render('home',{
-    //         title:"codeial home",
-    //         posts : post
-    //     });
-    // })
+module.exports.home = async function(req,res){
+    try{
 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path : 'comments',
-        populate : {
-            path : 'user'
-        }
-    })
-    .exec(function(err,post){
-        if(err){
-                    console.log('error in fetching');
-                    return;
-                }
+        let post = await Post.find({})
+        .populate('user')
+        .populate({
+            path : 'comments',
+            populate : {
+                path : 'user'
+            }
+        });
+        
 
-                User.find({},function(err,user){
-                    return res.render('home',{
-                        title:"codeial home",
-                        posts : post,
-                        all_users : user
-                    });
-                })
+        let user = await User.find({})
+
+        return res.render('home',{
+            title:"codeial home",
+            posts : post,
+            all_users : user
+        });
+    }catch(err){
+        console.log('ERROR',err);
+        return;
+    }
                 
-    })
 }
+
+
+//------------------------------------------------------promises method-----------------------------------------------------------------
+
+// module.exports.home = function(req,res){
+
+//     try{
+//         let post = Post.find({})
+//         .populate('user')
+//         .populate({
+//             path : 'comments',
+//             populate : {
+//                 path : 'user'
+//             }
+//         }).exec();
+    
+//         let user = post.then(User.find({}));
+
+//         user.then(function(){
+//             return res.render('home',{
+//                 title:"codeial home",
+//                 posts : post,
+//                 all_users : user
+//             });
+//         });
+       
+//     }catch(err){
+//         console.log('ERROR',err);
+//          return;
+//     }
+   
+// }
+
+//------------------------------------simple method-------------------------------------------------------------------------
+
+// module.exports.home = function(req,res){
+
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path : 'comments',
+//         populate : {
+//             path : 'user'
+//         }
+//     })
+//     .exec(function(err,post){
+//         if(err){
+//                     console.log('error in fetching');
+//                     return;
+//                 }
+
+                // User.find({},function(err,user){
+                //     return res.render('home',{
+                //         title:"codeial home",
+                //         posts : post,
+                //         all_users : user
+                //     });
+                // })
+                
+//     })
+// }
