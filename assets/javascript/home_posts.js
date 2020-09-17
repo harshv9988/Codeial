9fb4cbf150e025dc6489555bed872549cdc4cf35
harshv9988 <harshv9988@gmail.com>
2020-09-17 13:08:32 +0530

    posts ajax bug fixed

--- assets/javascript/home_posts.js
+++ assets/javascript/home_posts.js
@@ -39,57 +39,91 @@
     }
 
     let newPostDom = function(post){
-       return $(`
-       <div class="card my-4"  id="post-${post._id}">
-            <div class="card-header">
-                Featured
-            </div>
-            <div class="card-body">
-                <li">
-                    <p>
-                        
-                            <p>
-                                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
-                            </p>    
-                       
-                        <small>
-                        ${post.content}
-                        </small>
-                        <small>
-                        ${post.user.name}
-                        </small>
-                        <br>
-                        <small>
-                            
-                                <a class = "toggle-like-button" data-likes = "0" href="/likes/toggle/?id=${post._id}&type=Post">
-                                    0 Likes
-                                </a>    
+      return $(`
+      <div class="card m-4" id="post-${post._id}">
+      <div class="card-header">
+          <div class="d-flex flex-row justify-content-between">
+              <div class="d-flex flex-row profile-info">
+                  <h3 class="profile-pic-holder">
+                      
+                          <img src="${post.user.avatar}" alt="image">
+                      
+                  </h3>
+                  <h4 class="text-capitalize profile-name-holder">
+                      ${post.user.name}
+                  </h4>
+              </div>
+              <div class="dropdown">
+                  <a class="dropdown-toggle" type="button" id="dropdownMenuButton-${post._id}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v"></i>
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton-${post._id}">
+                     
+                          <a class="delete-post-button dropdown-item" href="/posts/destroy/${post._id}">Delete</a>
+                     
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="card-body">
+          
+              <p>
+                  <small>
+                      ${post.content}
+                  </small>
+                  <br>
+                  <small>
+                  </small>
+              </p>   
+              
+          
+      </div>
+     
+          
+          <div class="card-footer text-muted d-flex flex-row">
+              <div class="mx-2">
+                  
+                      
+                          <a class = "toggle-like-button" data-likes = "${post.likes.length}" href="/likes/toggle/?id=${post._id}&type=Post" style="text-decoration: none;">
+                              ${post.likes.length} <i class="far fa-thumbs-up"></i>
+                          </a>  
                            
-                                
-                            
-                        </small>
-                    </p>   
-                    <div>
-                       
-                            <form action="/comments/create" method="POST" id="post-${post._id}-comments-form">
-                                <input type="text" name="content" placeholder="Type here to comment">
-                                <input type="hidden" name="post" value="${post._id}">
-                                <input type="submit" value="Add Comment">
-                            </form>
-                       
-                    </div> 
-                
-                        
-                    <div class="post-comments-list">
-                        <ul id="post-comments-${post._id}">
-                            
-                        </ul>
-                    </div>
-                
-                </li>
-            </div>
-     </div>
-       `)
+                  
+              </div>
+              <div class="mx-2">
+                  <button class="btn btn-link btn-block" type="button" data-toggle="collapse" 
+              data-target="#post-${post._id}-collapse" aria-expanded="true" aria-controls="collapseOne" style="width:140px; text-decoration: none; padding-top: 2px;">
+                  <h4>
+                      <i class="fas fa-comment"></i>
+                      Comments
+                  </h4>
+                  </button>
+              </div>    
+          </div>
+              <div id="post-${post._id}-collapse" class="collapse card-footer" aria-labelledby="headingOne">
+                  <div class="card-body">
+                      <div>
+                          
+                              <form action="/comments/create" method="POST" id="post-${post._id}-comments-form">
+                                  <input type="text" name="content" placeholder="Type here to comment">
+                                  <input type="hidden" name="post" value="${post._id}">
+                                  <input type="submit" value="Add">
+                              </form>
+                          
+                      </div> 
+                  
+                          
+                      <div class="post-comments-list">
+                          <ul id="post-comments-${post._id}" style="list-style-type: none; padding: 0px;">
+                              
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          
+          
+    </div>
+      `)
         }
 
         //----------------------------------deleting post-------------------------------------
