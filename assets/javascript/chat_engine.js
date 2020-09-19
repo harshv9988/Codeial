8fd65cb43cc9b62e5f82d4a6f0cd7f8198ac0d1e
harshv9988 <harshv9988@gmail.com>
2020-09-19 09:15:56 +0530

    added basic chat engine

--- assets/javascript/chat_engine.js
+++ assets/javascript/chat_engine.js
@@ -11,8 +11,56 @@ class ChatEngine{
     }
 
     connectionHandler(){
+        let self = this;
+
         this.socket.on('connect',function(){
             console.log("Connection established using sockets...!");
+
+            self.socket.emit('join_room',{
+                user_email : self.userId,
+                chatroom : 'codeial'
+            });
+
+            self.socket.on('user_joined',function(data){
+                console.log('New User Joined',data);
+            });
+
+        });
+
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message : msg,
+                    user_email : self.userId,
+                    chatroom : 'codeial'
+                });
+            }
+        });
+
+        self.socket.on('receive_message',function(data){
+            console.log('message received', data.message);
+
+            let newMessage = $("<li>");
+            let messageType = 'other-message';
+
+            if(data.user_email==self.userId){
+                messageType = 'self-message';
+            }
+
+            newMessage.append($('<span>', {
+                'html': data.message
+            }));
+
+
+            newMessage.append($('<sub>', {
+                'html': data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
         })
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -6,6 +6,17 @@ module.exports.chatSockets = function(socketServer){
 
         socket.on('disconnect',function(){
             console.log('Connection Disconnected---------->');
+        });
+
+        socket.on('join_room',function(data){
+            console.log('joining request received',data);
+
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined',data);
+        })
+
+        socket.on('send_message',function(data){
+            io.in(data.chatroom).emit('receive_message',data);
         })
     });
 }
