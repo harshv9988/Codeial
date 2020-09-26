60c30a6850dc5a64dfa9371876924bfb44025a36
harshv9988 <harshv9988@gmail.com>
2020-09-26 08:28:12 +0530

    added bash profile

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,24 @@ const development = {
 }
 
 const production = {
-    name : 'production'
+    name : 'production',
+    asset_path : process.env.SOCIALARRAY_ASSET_PATH,
+    session_cookie_key : process.env.SOCIALARRAY_SESSION_COOKIE_KEY,
+    db: process.env.SOCIALARRAY_DB,
+    smtp : {
+        service : 'gmail',
+        host : 'smpt.gmail.com',
+        port: 587,
+        secure: false,
+        auth : {
+            user : process.env.SOCIALARRAY_GMAIL_USERNAME,
+            pass : '***'//hidden for sometime
+        }
+    },
+    google_client_id : process.env.SOCIALARRAY_GOOGLE_CLIENT_ID,
+    google_client_secret : process.env.SOCIALARRAY_GOOGLE_CLIENT_SECRET,
+    google_call_back_url : process.env.SOCIALARRAY_GOOGLE_CALLACK_URL,
+    jwt_secret : process.env.SOCIALARRAY_JWT_SECRET
 }
 
-module.exports = development;
+module.exports = eval(process.env.SOCIALARRAY_ENVIRONMENT) == undefined ? development : eval(process.env.SOCIALARRAY_ENVIRONMENT);
