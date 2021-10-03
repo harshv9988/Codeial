01659eb0cc5a5fbd270c522db16967fedaad539e
harshv9988 <harshv9988@gmail.com>
2021-10-03 17:45:56 +0530

    new deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
-const env = require('./environment');
+const mongoose = require("mongoose");
+const env = require("./environment");
 
-mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(
+  `mongodb+srv://mongoUser:mongoUser@cluster0.qyh17.mongodb.net/socialarray?retryWrites=true&w=majority`
+);
 
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'error in conncting to DB'));
+db.on("error", console.error.bind(console, "error in conncting to DB"));
 
-db.once('open',function(){
-    console.log('connected to database::MOngoDb');
+db.once("open", function () {
+  console.log("connected to database::MOngoDb");
 });
 
 module.exports = db;
